a123d5c5614782c15317e742fee3ed2091cb539a
astralaria <pawut.j@gmail.com>
2022-12-23 01:21:13 +0700

    Fix Port In test

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -9,7 +9,7 @@ import (
 
 func ConnectDB() *sql.DB {
 
-	url := "postgres://root:root@db/go-example-db?sslmode=disable"
+	url := "postgres://root:root@db:5433/go-example-db?sslmode=disable"
 	db, err := sql.Open("postgres", url)
 
 	if err != nil {
